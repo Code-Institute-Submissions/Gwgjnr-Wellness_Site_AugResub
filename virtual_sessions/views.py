8c490555e43b3bd7f0a5e6d0ddb58f91d25560d1
Gwgjnr <geoffwgg@gmail.com>
2022-05-22 16:45:37 +0000

    refactored templates for cancelling and joining seminars

--- virtual_sessions/views.py
+++ virtual_sessions/views.py
@@ -53,6 +53,7 @@ class JoinSeminar(View):
         seminar = get_object_or_404(Session, title=title)
         if request.user in seminar.signed_up.all():
             seminar.signed_up.remove(request.user)
+            messages.warning(request, f'You cancelled your place at {seminar.title}')
             seminar.save()
 
         else:
