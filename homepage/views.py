a66666d923b243b480698b98d2e121b8bff96268
Gwgjnr <geoffwgg@gmail.com>
2022-05-24 16:41:03 +0000

    added initial carousel function to homepage for featured section

--- homepage/views.py
+++ homepage/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from virtual_sessions.models import Session
 
 
 def index(request):
     """
     A view to return the homepage
     """
-    return render(request, 'homepage/index.html')
+    seminars = Session.objects.all()
+
+    context = {
+        'seminars': seminars,
+    }
+
+    return render(request, 'homepage/index.html', context)
